698d1a7698dad796392af0616faef27156c93722
ricksterhd123 <ricksterhd123@gmail.com>
2021-06-17 00:09:08 +0100

    sync with original changes

--- server.lua
+++ server.lua
@@ -234,7 +234,6 @@ local function acceptInvite(thePlayer)
 end
 addCommandHandler("teamaccept", acceptInvite)
 
-
 --- invite button has been pressed
 local function onPlayerInviteButtonPressed()
     triggerClientEvent(client, "teams:openPlayerList", resourceRoot, getPlayersWithoutTeam())
